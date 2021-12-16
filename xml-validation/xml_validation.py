eeb18fad1d69c3ef92476f13f4ab71dddb695baf
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-12-15 23:25:15 -0600

    Add metadata XML validation settings file

---
+++ xml-validation/xml_validation.py
@@ -0,0 +1,20 @@
+XML_VALIDATION = {
+    "http://www.openarchives.org/OAI/2.0/oai_dc/": "http://www.openarchives.org/OAI/2.0/oai_dc.xsd",
+
+    "http://www.lido-schema.org": "http://www.lido-schema.org/schema/v1.0/lido-v1.0.xsd",
+
+    "http://www.loc.gov/MARC21/slim": "https://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd",
+
+    "http://www.loc.gov/mods/v3": "https://www.loc.gov/standards/mods/mods.xsd",
+
+    "http://slubarchiv.slub-dresden.de/rights1": "https://slubarchiv.slub-dresden.de/slubarchiv/standards/rights/rights1.xsd",
+
+    "alto": "http://www.loc.gov/standards/alto/alto-v2.0.xsd",
+
+    "http://purl.org/dc/elements/1.1/": None,
+
+    "metadata": None,
+
+    "bag-info": None,
+}
+XML_VALIDATION_FAIL_ON_ERROR = False
