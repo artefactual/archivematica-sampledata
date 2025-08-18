2e44a9a974d0ed25cf17c5b62cd33c4fde5e68f0
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-08-18 04:17:25 +0000

    Upgrade LIDO schema

--- xml-validation/xml_validation.py
+++ xml-validation/xml_validation.py
@@ -5,7 +5,7 @@ __DIR = Path(__file__).parents[0] / "schemas"
 
 XML_VALIDATION = {
     "http://www.openarchives.org/OAI/2.0/oai_dc/": (__DIR / "oai_dc.xsd").as_posix(),
-    "http://www.lido-schema.org": (__DIR / "lido-v1.0.xsd").as_posix(),
+    "http://www.lido-schema.org": (__DIR / "lido-v1.1.xsd").as_posix(),
     "http://www.loc.gov/MARC21/slim": (__DIR / "MARC21slim.xsd").as_posix(),
     "http://www.loc.gov/mods/v3": (__DIR / "mods.xsd").as_posix(),
     "http://slubarchiv.slub-dresden.de/rights1": (__DIR / "rights1.xsd").as_posix(),
